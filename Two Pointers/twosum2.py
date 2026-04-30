68dbf9b56060814f87d10b319a7f2238d4b8e260
Deszmond <onyienujuba@gmail.com>
2026-04-30 16:32:29 +0100

    updated twosum.py

---
+++ Two Pointers/twosum2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        # Pattern: Two Pointers (Opposite Ends / Sorted Array)
+        # Key Idea: Exploit sorted order to shrink search space
+        # Time: O(n)
+        # Space: O(1)
+
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curr = numbers[l] + numbers[r]
+
+            if curr == target:
+                return [l + 1, r + 1]
+            elif curr > target:
+                r -= 1
+            else:
+                l += 1
+
+        return []  # fallback if no solution (not necessary tho)
